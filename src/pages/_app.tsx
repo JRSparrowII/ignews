cf7aad669621955a9521b6bc053dd703c4c45f20
Carlos Henrique <carloshenriquepvh@hotmail.com>
2022-08-30 09:35:13 -0400

    Autenticando o sistema com Login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
-// import { Provider as NextAuthProvider } from 'next-auth/client';
+import { Provider as NextAuthProvider } from 'next-auth/client';
 // import styles from './styles.module.scss'
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* // <NextAuthProvider session={pageProps.session} > */}
+      // <NextAuthProvider session={pageProps.session} >
         <Header />
         <Component {...pageProps} />
-      {/* // </NextAuthProvider> */}
+      // </NextAuthProvider>
     </>
   )
 }
--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -3,9 +3,9 @@ import {NextApiRequest, NextApiResponse} from 'next';
 
 export default (request: NextApiRequest, response:NextApiResponse ) => {
     const users = [
-        {id: 1, name: 'Nemesio', email: 'tomwilliams@gmail.com'},
-        {id: 2, name: 'Henrique', email: 'lindenwoams@gmail.com'},
-        {id: 3, name: 'Prisco', email: 'lindenwoams@gmail.com'},
+        {id: 1, name: 'Nemesio', email: 'Nemesio@gmail.com'},
+        {id: 2, name: 'Henrique', email: 'Henrique@gmail.com'},
+        {id: 3, name: 'Prisco', email: 'Prisco@gmail.com'},
     ]
     return response.json(users);
 };
